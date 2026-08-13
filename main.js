a34b7d71ed04ecebceb4b9890c1674c5639dc3ca
SilverFennec <123173551+SilverFennec@users.noreply.github.com>
2026-08-13 15:12:20 +0000

    Implement arithmetic operations and display updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-  return a + b;
+  return +a + +b;
 }
 
 function subtract(a, b) {
@@ -7,16 +7,15 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
+  if (b == '') {
+    return a;
+  }
   return a * b;
 }
 
 function divide(a, b) {
   return a / b;
 }
-
-let firstNum;
-let secondNum;
-let operator;
 
 function operate(a, b, operator) {
   switch (operator) {
@@ -32,8 +31,48 @@ function operate(a, b, operator) {
     case '/':
       return divide(a, b);
       break;
-    
+      
     default:
       return 'Enter a valid operator';
   }
 }
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let currentNum = 'firstNum'
+let result = '';
+
+const display = document.querySelector('.displayInput');
+const numpad = document.querySelector('.numpad');
+const operators = document.querySelector('.operators');
+const clearButton = document.querySelector('.clearButton');
+
+numpad.addEventListener('click', (event) => {
+  if (currentNum == 'firstNum') {
+    firstNum += event.target.value;
+    console.log(firstNum);
+    display.value = firstNum;
+  }
+  else if (currentNum == 'secondNum') {
+    secondNum += event.target.value;
+    console.log(secondNum);
+    display.value = secondNum;
+  }
+})
+
+operators.addEventListener('click', (event) => {
+  if (secondNum == '') {
+    operator = event.target.value;
+    currentNum = 'secondNum';
+  }
+  if (secondNum !== '') {
+    display.value = operate(firstNum, secondNum, operator)
+    result = display.value;
+    firstNum = result;
+    secondNum = '';
+    currentNum = 'secondNum'
+    
+    operator = event.target.value;
+  }
+})
